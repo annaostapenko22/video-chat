ab7db41c9085b3fe19a2f32f0099a562daa7123e
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-10-17 12:26:21 +0200

    finished

--- server.js
+++ server.js
@@ -3,28 +3,31 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const {v4: uuisV4} = require('uuid')
+const { v4: uuisV4 } = require("uuid");
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.redirect(`/${uuisV4()}`)
+  res.redirect(`/${uuisV4()}`);
 });
-
 
 app.get("/:room", (req, res) => {
   console.log(" req.params.room", req.params.room);
   res.render("room", { roomId: req.params.room });
 });
 
-io.on("connection", soscket=> {
-    soscket.on("join-room", (roomId, userId)=> {
-        console.log("here",roomId, userId)
-        soscket.join(roomId)
-        soscket.to(roomId).broadcast.emit("user-connected", userId)
-    })
-})
+io.on("connection", (soscket) => {
+  soscket.on("join-room", (roomId, userId) => {
+    console.log("here", roomId, userId);
+    soscket.join(roomId);
+    soscket.to(roomId).broadcast.emit("user-connected", userId);
+
+    soscket.on("disconnect", () => {
+      soscket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
 
 server.listen(3300);
